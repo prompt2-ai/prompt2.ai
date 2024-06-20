5ba9b803d65ead9d53c0c5061f3b2b49eaf1e1bb
root <root@skarvelis.gr>
2024-06-20 17:39:32 +0300

    change path

--- doc.theme.config.jsx
+++ doc.theme.config.jsx
@@ -1,9 +1,9 @@
 export default {
     logo: <span>Prompt2.ai Documentation</span>,
     project: {
-      link: 'https://github.com/prompt2-ai/prompt2'
+      link: 'https://github.com/prompt2-ai/prompt2.ai'
     },
-    docsRepositoryBase:'https://github.com/prompt2-ai/prompt2/tree/main'
+    docsRepositoryBase:'https://github.com/prompt2-ai/prompt2.ai/tree/main'
     // ... other theme options
      
   }
