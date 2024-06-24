a9bb90b086c2d71238ff4329328d82afe503bf40
root <root@skarvelis.gr>
2024-06-24 19:46:20 +0300

    chore: Update layout to include sticky menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,10 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <div className="shadow-2xl inset-y-0 min-h-7 sticky text-center bg-red-500 z-40"><strong>WARNING! this site is under heavy development.</strong></div>
+      <div className="flex sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <Menu session={session}/></div>
       <main className="container isolate mx-auto px-4">
-        <Menu session={session}/>
+        
         <div className="flex flex-col min-h-screen p-8">
           {children}
         </div>
