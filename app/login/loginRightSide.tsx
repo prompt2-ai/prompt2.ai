10a16eb61beb8602b7ac4b4022b30746de5b20b2
root <root@skarvelis.gr>
2024-06-24 11:41:22 +0300

    feat: Add login page components
    - Added LoginRightSide component for the right side of the login page
    - Added LoginLeftSide component for the left side of the login page
    - Updated LoginRightSide to include UserAuthForm and legal links
    - Updated LoginLeftSide to include AiDataMesh component and testimonial

---
+++ app/login/loginRightSide.tsx
@@ -0,0 +1,37 @@
+import UserAuthForm from '@/app/login/form';
+import Link from "next/link"
+
+export default function LoginRightSide() {  
+    return (
+        <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an Account/Login in
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to login in.
+            </p>
+          </div>
+          <UserAuthForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/legal/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="legal/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    )
+}
