beafae3d91d75ee421eb1f0d8d7bf1fdf0086a90
root <root@skarvelis.gr>
2024-06-27 22:23:26 +0300

    refactor: Update auth.ts to use SequelizeAdapter for database connectivity and jwt as session management

--- auth.ts
+++ auth.ts
@@ -2,7 +2,21 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
+import SequelizeAdapter from "@auth/sequelize-adapter"
+import { Sequelize } from "sequelize"
+import mysql from 'mysql2';
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   debug: true,
+  adapter: SequelizeAdapter(
+    new Sequelize(process.env.DATABASE_URL || 'mysql://root:pass@localhost:3306/prompt2',{
+      dialectModule: mysql,
+      dialect: 'mariadb',
+      logging: false// for enable set console.log not true,
+    })
+  ),
+  session: {
+     strategy:'jwt', //to be able to run on edge midleware we cant use database to store session
+  },
 });
