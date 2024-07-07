8948dd10978db3336c97358aaef69c4d72f49a7b
root <root@skarvelis.gr>
2024-07-07 22:56:18 +0300

    feat: Add dashboard page with sidebar navigation and workflow table

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+export default function Page() {
+
+    return (
+        <div>
+            have a left side bar with the following links:
+            <ul>
+                <li><Link href="/dashboard/bpmn">New Workflow</Link></li>
+                <li>Settings</li>
+            </ul>
+            and a table to display the workflows created by the user with the following columns:
+            image, name, description, date created, date modified, and actions
+            
+            <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+        </div>
+    );
+}
+
