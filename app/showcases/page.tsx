76c5d9a0502a38f9d3605f50d435d995ff4988af
root <root@skarvelis.gr>
2024-07-07 22:56:10 +0300

    feat: Add showcases page with list of workflows

---
+++ app/showcases/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+
+
+export default function Page() {
+
+    return (
+        <div>
+            <h1>Showcases, a list with workflows with pagination and search</h1>
+        </div>
+    );
+}
