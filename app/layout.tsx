e3e7cf410fae4a21eaf56135eca9ef5535e0b07c
root <root@skarvelis.gr>
2024-07-09 23:36:45 +0300

    refactor: Update layout component to include new theme.css and make styling adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Menu from "@/components/custom/menu";
 import Footer from "@/components/custom/footer";
 import Sidebar from "@/components/custom/sidebar";
 import "./globals.css";
+import './theme.css';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,11 +29,11 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <div className="shadow-2xl inset-y-0 min-h-7 sticky text-center bg-red-500 z-40"><strong>WARNING! this site is under heavy development.</strong></div>
-      <div className="flex sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="flex sticky top-0 z-50 w-full border-b border-border/40 bg-black backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <Menu session={session}/></div>
       <Sidebar session={session} className="hidden md:flex w-16 h-full bg-background/95 border-r border-border/40 p-4"/>
-      <main className="container isolate mx-auto px-4">
-        <div className="flex flex-col min-h-screen p-8">
+      <main className="w-full">
+        <div className="flex flex-col lg:min-h-screen p-8">
           {children}
         </div>
        </main>
