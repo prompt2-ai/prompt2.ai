7a6ff9a9f538a6d203f501d491710400feef8a6e
root <root@skarvelis.gr>
2024-07-09 23:38:31 +0300

    refactor: Update landing page with lazy loading for header image and video player

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,17 @@
 "use client";
-import Image from "next/image";
+import HeaderImage from "../public/header.jpg";
+import Player from 'next-video/player';
+
 
 export default function Home() {
   return (
-<main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-      Welcome to&nbsp;
-      <code className="font-mono font-bold">prompt2.ai</code><br/>
-      </p>
-      <hr />
-      <h1 className="font-mono">"prompt to anything, soon..."</h1>
-  </div>
-
-  <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-  </div>
-
-  <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-    {/* You can replace the following sections with your own content */}
+<main className="lg:bg-gradient-to-b from-orange-300 to-orange-700 lg:shadow-2xl lg:p-32 w-full">
+  <div className="z-10 md:flex items-center justify-center">
+     <Player 
+     src="https://prompt2.ai/headerVideo.mp4" 
+     poster={HeaderImage.src}
+     blurDataURL={HeaderImage.blurDataURL}
+     className="lg:border-2 md:border-8 md:border-black lg:border-white"/>
   </div>
 </main>
   );
