786ffd36e3bdfd555d4df3ea10065056b30cfb47
root <root@skarvelis.gr>
2024-07-10 13:38:00 +0300

    typo

--- components/custom/footer.tsx
+++ components/custom/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="https://prompt2.io/" className="flex items-center">
+              <a href="https://prompt2.ai/" className="flex items-center">
                   <Image src="/logo.svg" 
                   className="h-8 me-3" 
                   width={50} 
@@ -52,7 +52,7 @@ const Footer = () => {
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://prompt2.io/" className="hover:underline">Prompt2</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://prompt2.ai/" className="hover:underline">Prompt2</a>. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
