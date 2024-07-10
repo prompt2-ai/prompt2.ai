0dafe70956833eff24404572215d9627eeff8414
root <root@skarvelis.gr>
2024-07-10 12:40:56 +0300

    refactor: Update loginRightSide component to improve responsiveness and styling

--- app/login/loginRightSide.tsx
+++ app/login/loginRightSide.tsx
@@ -7,8 +7,8 @@ import { Button } from "@/components/ui/button"
 
 export default function LoginRightSide() {  
     return (
-        <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="sm:p-2 md:p-4 lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center items-center space-y-6">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Login in
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,14 +8,13 @@ export default function LoginPage() {
 
   return (
     <>
-
-    <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow">
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center justify-between h-full md:grid sm:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="overflow-hidden md:rounded-[0.5rem] md:border bg-background md:shadow">
+      <div className="lg:container lg:relative lg:h-[800px] lg:flex-col lg:items-center lg:justify-center lg:justify-between lg:h-full sm:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/login"
           className={cn(
             buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
+            "hidden lg:block absolute right-4 top-4 md:right-8 md:top-8"
           )}
         >
           Login
