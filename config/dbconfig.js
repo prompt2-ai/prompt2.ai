2b42dd5106fd7ba946af9ea42a4e4db18c46b974
root <root@skarvelis.gr>
2024-07-13 16:19:25 +0300

    feat: Update database configuration to read .env file based on script invocation
    - Modify the `dbconfig.js` file to conditionally read the `.env` file from `../.env.local` based on whether the script is called from the command line or required as a module
    - Set the `isOnBash` flag to `true` when called from the command line
    - Update the `development` and `test` configurations to use the `isOnBash` flag to determine the `host` value
    - Add a `connectTimeout` option to the `dialectOptions` for the `development`, `test`, and `production` configurations
    - Enable logging by setting `logging` to `console.log` for the `development`, `test`, and `production` configurations

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,8 +1,11 @@
+let isOnBash = false;
 
-//read the .env file from ../.env.local
+//read the .env file from ../.env.local if this script called from command line
+if (require.main.loaded) { //when called from sequelize-cli loaded is true
+console.log('called from command line');
+isOnBash = true;
 const fs = require('fs');
 const path = require('path');
-
 const envPath = path.resolve(__dirname, '../.env.local');
 const env = fs.readFileSync(envPath, 'utf8');
 const envLines = env.split('\n');
@@ -10,16 +13,22 @@ envLines.forEach((line) => {
   const [key, value] = line.split('=');
   process.env[key] = value;
 });
+} else {
+console.log('required as module');
+}
 
 module.exports = {
   development: {
     username: process.env.MARIADB_USER,
     password: process.env.MARIADB_PASSWORD,
     database: process.env.MARIADB_DATABASE,
-    host: "localhost",
+    host: isOnBash?"localhost":process.env.MARIADB_HOST,
     port: process.env.MARIADB_PORT,
     dialect: 'mariadb',
-    dialectOptions: {}
+    dialectOptions: {
+      connectTimeout: 1000
+    },
+    logging: console.log// for enable set console.log not true,
   },
   test: {
     username: process.env.MARIADB_USER,
@@ -28,7 +37,8 @@ module.exports = {
     host: process.env.MARIADB_HOST,
     port: process.env.MARIADB_PORT,
     dialect: 'mariadb',
-    dialectOptions: {}
+    dialectOptions: {},
+    logging: console.log// for enable set console.log not true,
   },
   production: {
     username: process.env.MARIADB_USER,
@@ -37,6 +47,7 @@ module.exports = {
     host: process.env.MARIADB_HOST,
     port: process.env.MARIADB_PORT,
     dialect: 'mariadb',
-    dialectOptions: {}
+    dialectOptions: {},
+    logging: console.log// for enable set console.log not true,
   }
 };
