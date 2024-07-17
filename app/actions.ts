50610cdc10dea1ef1e370a91e3d344711fe74e7a
root <root@skarvelis.gr>
2024-07-17 10:03:40 +0300

    nip

--- app/actions.ts
+++ app/actions.ts
@@ -5,7 +5,7 @@ import { authConfig } from "@/auth.config"
 export const getSession=async ()=>{
   const auth = NextAuth(authConfig).auth;
   return auth().then((session) => {
-    console.log('session on getSession', session);
+    //console.log('session on getSession', session);
   return session;
   });
 }
