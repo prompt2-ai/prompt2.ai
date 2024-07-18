7ce07bf9bea5beb87fd6bac31479656df09c3ee0
root <root@skarvelis.gr>
2024-07-18 22:39:44 +0300

    placeholder

---
+++ app/O/Profile/page.tsx
@@ -0,0 +1,10 @@
+/* hese user can change name and can add the Gemini API key */
+'use client';
+export default function Profile() {
+
+    return (
+        <div>
+            <h1>TODO</h1>
+        </div>
+    );
+}
