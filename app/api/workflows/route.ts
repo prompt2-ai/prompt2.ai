a21e88d95e02d53b3216e5014539b7f3cce77cd4
root <root@skarvelis.gr>
2024-07-19 15:36:59 +0300

    refactor: Update route to allow updating workflows

--- app/api/workflows/route.ts
+++ app/api/workflows/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 import db from "@/db";
 import { auth } from "@/auth"
 
+
+
 export async function GET(request: NextRequest) {
     const session = await auth();
     if (!session?.user) {
@@ -39,4 +41,71 @@ export async function GET(request: NextRequest) {
             status: 200
         }
     );
+}
+
+type Workflow = {
+  id: string;
+  name: string;
+  description: string;
+  userId: string;
+  workflow: string;
+  image: string;
+  prompt: string;
+  active: boolean;
+  private: boolean;
+  tokensInput: number;
+  tokensOutput: number;
+  createdAt: Date;
+  updatedAt: Date;
+  update: (attributes: Partial<Workflow>) => Promise<void>;
+} | null;
+
+export async function PUT(request: NextRequest) {
+  //upade the workflow with attributes given in the body
+  const session = await auth();
+  if (!session?.user) {
+    return NextResponse.json(
+      {
+        error: {
+          code: "no-access",
+          message: "You are not signed in.",
+        },
+      },
+      { status: 401 }
+    );
+  }
+  const { id, ...attributes } = await request.json();
+  const workflow: Workflow | null = await db.Workflows.findByPk(id) as Workflow;
+  console.log(attributes);
+
+  if (!workflow) {
+    return NextResponse.json(
+      {
+        error: {
+          code: "not-found",
+          message: "Workflow not found.",
+        },
+      },
+      { status: 404 }
+    );
+  }
+  if (workflow.userId !== session.user.id) {
+    return NextResponse.json(
+      {
+        error: {
+          code: "no-access",
+          message: "You are not authorized to update this workflow.",
+        },
+      },
+      { status: 403 }
+    );
+  }
+  await workflow.update(attributes);
+  return NextResponse.json(
+    {
+      workflow,
+    },
+    { status: 200 }
+  );
+  
 }
