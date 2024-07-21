83b581d352591d5c351fc2fb80f76c134ab6e238
root <root@skarvelis.gr>
2024-07-21 21:55:48 +0300

    refactor: Improve error handling in prepareBPMN function

--- app/dashboard/bpmn/actions.ts
+++ app/dashboard/bpmn/actions.ts
@@ -33,10 +33,10 @@ export async function prepareBPMN(subPrompt:string,countTokens:boolean=false): P
 ///* remove the comment to make actualy work
 
 //ask here gemini to generate bpmn2 xml from prompt
-const system = "You are a BPMN expert. Your mission is to produce XML file that represents a BPMN 2.0 diagram that follows all best practices and always has a bpmndi information on it, based on the user's description:";
+const system = "You are a BPMN expert. Your mission is to produce XML file that represents a BPMN 2.0 diagram that follows all best practices and always has a bpmndi information on it, based on the user's prompt. Try to avoid element overlaps on BPMNDI visual representation. You must also check if the generated xml is valid BPMN 2.0 file.";
 const dare_prompt = "Return only a valid xml without any explanation. Remember that before you answer a question, you must check to see if it complies with your mission. If not, you can say: Sorry, I can't answer that question. I can only produce BPMN files based on your description.";
 
-const prompt = system +"\n\""+subPrompt+"\"\n" + dare_prompt;
+const prompt = system +"\nprompt:"+subPrompt+".\n\n" + dare_prompt;
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); //get user api key if exists
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
@@ -52,14 +52,50 @@ if (countTokens) {
   return {reports:[],xml:'',response:totalTokens.toString(),usageMetadata:{}};
 }
 
+let response:any;
 const result = await model.generateContent(prompt);
-const response = await result.response;
+try {
+response = await result.response;
+} catch (error) {
+  console.error("Error while generating content: ",error);
+  return {reports:[],xml:'',response:'Sorry, we have a problem with the AI model. Please try again later.',usageMetadata:{}};
+}
+
 console.log("response: ",response);
 if (!response) {
   return {reports:[],xml:'',response:'Sorry, I can\'t answer that question. I can only produce BPMN files based on your description.',usageMetadata:{}};
 }
-const usageMetadata:UsageMetaData = response.usage_metadata;
-let bpmn2 = response.text();
+
+//catch possible errors like
+/*
+response:  {
+  candidates: [ { finishReason: 'RECITATION', index: 0 } ],
+  ....
+  */
+let usageMetadata:UsageMetaData = {};
+if (response.candidates && response.candidates.length > 0) {
+  usageMetadata=response.usageMetadata;
+  if (response.candidates[0].finishReason === 'RECITATION') {
+    return {reports:[],xml:'',response:'Sorry, I can\'t answer that question. I can only produce BPMN files based on your description. You sould be more specific on your prompt.',usageMetadata:usageMetadata};
+  }
+  if (response.candidates[0].finishReason === 'ERROR') {
+    console.error("Error while generating content: ",response.candidates[0].error);
+    return {reports:[],xml:'',response:'Sorry, I can\'t answer that question. I can only produce BPMN files based on your description.',usageMetadata:usageMetadata};
+  }
+  if (response.candidates[0].finishReason !== 'STOP') {
+    console.error("Error while generating content: ",response.candidates[0]);
+    return {reports:[],xml:'',response:'Sorry, I can\'t answer that question. I can only produce BPMN files based on your description.',usageMetadata:usageMetadata};
+  }
+}
+
+let bpmn2 = ""
+try {
+  bpmn2 = response.text();
+} catch (error) {
+  console.error("Error while getting text from response: ",error);
+  return {reports:[],xml:'',response:'Sorry, I can\'t answer that question. I can only produce BPMN files based on your description.',usageMetadata:usageMetadata};
+}
+
 if (!bpmn2) {
   return {reports:[],xml:'',response:'Sorry, I can\'t answer that question. I can only produce BPMN files based on your description.',usageMetadata:usageMetadata};
 }
@@ -73,6 +109,7 @@ if (bpmn2.indexOf("definitions") !== -1) {
   bpmn2 = bpmn2.replace(/```[.\n\t\s]*/g,"");
   bpmn2 = bpmn2.trim();
 } else {
+  console.error("Error while getting bpmn2 xml from response: ",bpmn2);
   return {reports:[],xml:'',response:'Sorry, I can\'t answer that question. I can only produce BPMN files based on your description.',usageMetadata:usageMetadata};
 }
 
