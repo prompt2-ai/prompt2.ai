8d49a835ab53d244d60f4a790b0e79d35a12b766
root <root@skarvelis.gr>
2024-07-22 18:46:07 +0300

    refactor: Add API route for getting available tokens for the user

---
+++ app/api/tokens/route.ts
@@ -0,0 +1,63 @@
+// get available tokens for the user
+import { NextResponse } from "next/server";
+import { Op } from '@sequelize/core';
+import db from "@/db";
+import { auth } from "@/auth"
+
+export async function GET() {
+    const session = await auth();
+    if (!session?.user) {
+      return NextResponse.json(
+        {
+          error: {
+            code: "no-access",
+            message: "You are not signed in.",
+          },
+        },
+        { status: 401 }
+      );
+    }
+    //first get use subscription status and start date and if user is subscriber
+    //stripe_current_period_start and stripe_current_period_end
+
+const paidTokens = await db.Tokens.sum('value',{
+    where: {
+        userId: session.user.id,
+        expires: {
+            [Op.gte]: new Date(),
+        },
+    },
+});
+
+const oldestCreatedTokenDate = await db.Tokens.min('createdAt',{
+    where: {
+        userId: session.user.id,
+        expires: {
+            [Op.gte]: new Date(),
+        },
+    },
+});
+
+const usedTokens = await db.Prompts.sum('total_token_count',{
+    where: {
+      userId: session.user.id, // Filtering by user ID
+      spendAt: {
+        [Op.lte]: new Date(), // spendAt is less than or equal to the current date
+      },
+      createdAt: {
+        [Op.gte]: oldestCreatedTokenDate, // createdAt is greater than or equal to the oldest token creation date
+      },
+    }
+  });
+
+
+return NextResponse.json(
+    {
+        paidTokens:paidTokens||0,
+        usedTokens:usedTokens||0,
+        availableTokens:(paidTokens||0) - (usedTokens||0),
+    },
+    { status: 200 }
+);
+
+}
