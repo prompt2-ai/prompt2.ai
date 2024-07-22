09912b367a19b40f524b23c562d0c45b1a9783d0
root <root@skarvelis.gr>
2024-07-22 18:46:20 +0300

    refactor: Add subscription creation and update logic in webhook route

--- app/api/webhooks/stripe/subscription/route.ts
+++ app/api/webhooks/stripe/subscription/route.ts
@@ -1,27 +1,61 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
 import db from "@/db";
+import { v4 as uuidv4 } from 'uuid'
 
 const webhookSecret: string = process.env.STRIPE_WEBHOOK_SUBSCRIPTION_SECRET!;
+
+
+const createSubscription = async (subscription: Stripe.Subscription) => {
+           const stripeSubscriptionId = subscription.id;
+           const stripeCurrentPeriodEnd = subscription.current_period_end*1000;//fix from unix timestamp to mysql timestamp
+           const stripeCurrentPeriodStart = subscription.current_period_start*1000;//fix from unix timestamp to mysql timestamp
+           const plan = subscription.items.data[0].price.recurring?.interval;
+  
+           //get userId from stripeCustomerId from Users table
+
+           const user = await db.User.findOne({ where: { stripeCustomerId:subscription.customer as string } });
+           //add tokens to user, 100K tokens for monthly, 1M tokens for yearly
+           const tokens = plan === "month" ? 100000 : 1000000;
+           try {
+             await db.Tokens.create( //updatedAt,createdAt added by sequelize
+               {
+                 id:uuidv4(),
+                 userId: user?.get("id"),
+                 value: tokens,
+                 purchasedAt: new Date(),
+                 source: "subscription",
+                 validFrom: new Date(stripeCurrentPeriodStart),
+                 expires: new Date(stripeCurrentPeriodEnd)
+                }
+             );
+             console.log("User updated with tokens on subscription creation - customer: %s, subId: %s, tokens:%s",subscription.customer, stripeSubscriptionId, tokens);
+           } catch (error) {
+             console.error("Error updating user with tokens on subscription creation", error);
+             return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+           }
+}
 
 const updateSubscription = async (subscription: Stripe.Subscription) => {
            //const customer = await stripe.customers.retrieve(subscription.customer as string);
            const stripeSubscriptionId = subscription.id;
            const stripePriceId = subscription.items.data[0].price.id;
            const stripeCurrentPeriodEnd = subscription.current_period_end*1000;//fix from unix timestamp to mysql timestamp
+           const stripeCurrentPeriodStart = subscription.current_period_start*1000;//fix from unix timestamp to mysql timestamp
            const role = "subscriber";
            const plan = subscription.items.data[0].price.recurring?.interval;
            //update user with stripeSubscriptionId, role, plan, stripe_current_period_end, stripe_price_id, is_active
            try {
              await db.User.update( //updatedAt added by sequelize
-              { stripeSubscriptionId, role, plan, stripeCurrentPeriodEnd, stripePriceId },
+              { stripeSubscriptionId, role, plan,stripeCurrentPeriodStart, stripeCurrentPeriodEnd, stripePriceId },
               { where: { stripeCustomerId:subscription.customer as string } }
             );
-            console.log("User updated on subscription creation/update  - customer: %s, subId: %s, role:%s, scpe:%s, spId:%s",subscription.customer, stripeSubscriptionId, role, plan, stripeCurrentPeriodEnd, stripePriceId); 
+            console.log("User updated on subscription update  - customer: %s, subId: %s, role:%s, scpe:%s, spId:%s",subscription.customer, stripeSubscriptionId, role, plan, stripeCurrentPeriodEnd, stripePriceId); 
           } catch (error) {
-            console.error("Error updating user on subscription creation/update", error);
+            console.error("Error updating user on subscription update", error);
             return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
            }
+
 }
 
 const cancelSubscription = async (subscription: Stripe.Subscription) => {
@@ -71,12 +105,15 @@ export const POST = async (req: NextRequest) => {
     const subscription = event.data.object as Stripe.Subscription;
     switch (event.type) {
       case "customer.subscription.updated":
+        console.log("subscription updated");
         await updateSubscription(subscription);
         break;
       case "customer.subscription.created":
-        await updateSubscription(subscription);
+        console.log("subscription created");
+        await createSubscription(subscription);//add tokens to user
         break;
       case "customer.subscription.deleted":
+        console.log("subscription deleted");
         await cancelSubscription(subscription);
         break;
       default:
