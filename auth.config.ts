2e52d22c6fcf548a4cacab06b8e0082d94702e8b
root <root@skarvelis.gr>
2024-08-04 21:07:22 +0300

    refactor: Update next-auth to version 5.0.0-beta.20 and add Apple provider

--- auth.config.ts
+++ auth.config.ts
@@ -3,6 +3,7 @@ import type { NextAuthConfig } from 'next-auth';
 //import Credentials from 'next-auth/providers/credentials';
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import Apple from "next-auth/providers/apple"
 
 //import Linkedin from "next-auth/providers/linkedin"
 //import Resend from "next-auth/providers/resend"
@@ -33,14 +34,50 @@ function getUser(email: string): User | undefined {
 }
 */
 export const authConfig = {
+  cookies: {
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+     },
+  },
   pages: {
     signIn: '/login',
   },
   providers: [
     GitHub,
     Google,
+    Apple({
+      clientId: process.env.AUTH_APPLE_ID,
+      clientSecret: ""+process.env.AUTH_APPLE_SECRET,
+      checks: ["pkce"],
+      token: {
+        url: `https://appleid.apple.com/auth/token`,
+      },
+      client: {
+        token_endpoint_auth_method: "client_secret_post",
+      },
+      authorization: {
+        params: {
+          response_mode: "form_post",
+          response_type: "code",//do not set to "code id_token" as it will not work
+          scope: "name email"
+        },},
+        profile(profile) {
+          return {
+            id: profile.sub,
+            name: "Person Doe",//profile.name.givenName + " " + profile.name.familyName, but apple does not return name...
+            email: profile.email,
+            image: "",
+          }
+        }
+    }),
+
     /* Uncomment to enable other providers
-    Apple,
     Linkedin,
     Resend({
       from: "no-reply@prompt2.ai",
