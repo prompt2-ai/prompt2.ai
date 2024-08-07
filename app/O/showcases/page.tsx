2392faffd035b8711dee78ef6fd2f6419c52a3c6
root <root@skarvelis.gr>
2024-08-07 23:20:46 +0300

    feat: Add lazy loading for images in showcases page

--- app/O/showcases/page.tsx
+++ app/O/showcases/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-
-
+import React, { useState, useEffect } from 'react';
+import type { Workflow, Workflows } from '@/types/workflow';
 
 export default function Page() {
-
+    const [workflows, setWorkflows] = useState([] as Workflows);
     return (
         <div>
             <h1>TODO Showcases, a list with workflows with pagination and search</h1>
