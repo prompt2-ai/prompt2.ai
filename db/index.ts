ee45020eb72412d96652c20594b54b370f10aaae
root <root@skarvelis.gr>
2024-08-16 13:33:37 +0300

    staticaly import Op to prevent Critical dependency: the request of a dependency is an expression from webpack

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
 'use strict';
-import{ Sequelize,DataTypes,Options } from "sequelize"
-import { Op } from '@sequelize/core';
-import {production,test,development} from "../config/dbconfig.js"
+import{ Sequelize,DataTypes, Options, Op } from "sequelize"
+import { production, test, development } from "../config/dbconfig.js"
+import mysql2 from 'mysql2'; // Needed to fix sequelize issues with WebPack
 
 import UserModel  from "@/db/models/users"
 import Workflows from "@/db/models/workflows"
@@ -10,16 +10,22 @@ import Prompts from "@/db/models/prompts"
 
 const runningenv = process.env.NODE_ENV || 'development';
 //load the config based on the running environment
-export const config = runningenv === 'production' ? production : runningenv === 'test' ? test : development;
-const sequelize = new Sequelize({...config as Options});
-export const db = {
+const config = runningenv === 'production' ? production : runningenv === 'test' ? test : development;
+const options = {
+    ...config,
+    dialect: 'mysql',// overide the default dialect, just in case
+    dialectModule: mysql2,
+}
+
+const sequelize = new Sequelize({...options as Options});
+const db = {
     User: UserModel(sequelize, DataTypes),
     Workflows: Workflows(sequelize, DataTypes),
     Tokens: Tokens(sequelize, DataTypes),
     Prompts: Prompts(sequelize, DataTypes),
     sequelize: sequelize,
     Sequelize: Sequelize,
-    Op: Op
+    Op: Op,
 }
 
 export default db;
